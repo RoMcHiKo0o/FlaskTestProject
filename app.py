7a93085d66f8d0322e8881c5d1b4c342afb66607
roman <roman.golubovich@gmail.com>
2023-02-16 01:10:07 +0300

    todo normal json

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-import json
-
 from flask import Flask
 from crud import *
 from database import db
+from bson.json_util import dumps
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return json.dumps(get_cards(db))
+    return get_cards(db)
 
 
 if __name__ == "__main__":
--- crud.py
+++ crud.py
@@ -1,9 +1,11 @@
 def serialize(doc):
+    for i, j in doc:
+        print(i, j)
     return [{**i, "_id": str(i["_id"])} for i in doc]
 
 
 def get_cards(db):
-    return serialize(db.cards.find())
+    return [i for i in db.cards.find()]
 
 
 def get_card_by_id(db, id):
