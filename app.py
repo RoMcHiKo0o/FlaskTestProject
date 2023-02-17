2b7c8a8fd07f1264efef2b6d6236443266c3635e
roman <roman.golubovich@gmail.com>
2023-02-17 13:41:24 +0300

    added more crud (delete rollback, get cards, get card by id, activate/deactivate). Made correct json serializing

--- app.py
+++ app.py
@@ -1,14 +1,52 @@
 from flask import Flask
 from crud import *
 from database import db
-from bson.json_util import dumps
+from bson import json_util
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return get_cards(db)
+    return json_util.dumps({'message': get_cards(db)})
+
+
+@app.route('/cards/')
+def getCardsList():
+    res = get_cards_list(db)
+    print(res)
+    return json_util.dumps({"message": res})
+
+
+@app.route('/cards/<number>/')
+def getCard(number):
+    card = get_card_by_number(db, number)
+    print(card)
+    return json_util.dumps({"message": card})
+
+
+@app.route('/cards/<number>/activate/')
+def activateCard(number):
+    res = activate_card(db, number)
+    return json_util.dumps({"message": res})
+
+
+@app.route('/cards/<number>/deactivate/')
+def deactivateCard(number):
+    res = deactivate_card(db, number)
+    return json_util.dumps({"message": res})
+
+
+@app.route('/cards/<number>/delete/')
+def deleteCard(number):
+    res = delete_card(db, number)
+    return json_util.dumps({"message": res})
+
+
+@app.route('/cards/<number>/rollback/')
+def rollbackCard(number):
+    res = rollback_card(db, number)
+    return json_util.dumps({"message": res})
 
 
 if __name__ == "__main__":
--- crud.py
+++ crud.py
@@ -1,16 +1,81 @@
-def serialize(doc):
-    for i, j in doc:
-        print(i, j)
-    return [{**i, "_id": str(i["_id"])} for i in doc]
+from database import client
+
+DEFAULT_FIELDS = {'_id': 0, 'series': 1, 'number': 1, 'create_date': 1, 'end_date': 1, 'card_state': 1}
 
 
 def get_cards(db):
-    return [i for i in db.cards.find()]
+    return [i for i in db.cards.find({}, {'_id': 0})]
 
 
-def get_card_by_id(db, id):
-    pass
+def get_cards_list(db, filter_search=None, fields=None):
+    if fields is None:
+        fields = DEFAULT_FIELDS
+    if filter_search is None:
+        fields = {}
+    return [i for i in db.cards.find(filter_search, fields)]
 
 
-def update_card(db, id, newcard):
-    pass
+def get_card_by_number(db, number):
+    return next(db.cards.find({'number': number}), {})
+
+
+def get_card_state(db, number):
+    card = db.cards.find({'number': number}, {"_id": 0, 'card_state': 1})
+    return list(card)[0]['card_state']
+
+
+def activate_card(db, number):
+    state = get_card_state(db, number)
+    if state == "Not activated":
+        db.cards.update_one({'number': number}, {'$set': {'card_state': "Activated"}})
+        return "Card was successfully activated"
+    elif state == "Expired":
+        return 'Card is expired'
+    else:
+        return 'Card is already activated'
+
+
+def deactivate_card(db, number):
+    state = get_card_state(db, number)
+    if state == "Activated":
+        db.cards.update_one({'number': number}, {'$set': {'card_state': "Not activated"}})
+        return "Card was successfully deactivated"
+    elif state == "Expired":
+        return 'Card is expired'
+    else:
+        return 'Card is not activated'
+
+
+def delete_card(db, number):
+    card = get_card_by_number(db, number)
+    if card == {}:
+        return 'No card with that number'
+    db.deleted_cards.insert_one(card)
+    db.cards.delete_one({"number": number})
+    return "card was deleted"
+
+
+def get_deleted_card_by_number(db, number):
+    return next(db.deleted_cards.find({'number': number}), {})
+
+
+def rollback_card(db, number):
+    card = get_deleted_card_by_number(db, number)
+    if card == {}:
+        return 'There is no deleted card with that number'
+    res = insert_deleted_card(db, card)
+    if card == res:
+        db.deleted_cards.delete_one({"number": card['number']})
+        return "card was restored"
+    else:
+        return res
+
+
+def insert_deleted_card(db, card):
+    filter_search = {"$or": [{"_id": card['_id']}, {'series': card['series'], 'number': card['number']}]}
+    res = get_cards_list(db, filter_search=filter_search)
+    if not res:
+        db.cards.insert_one(card)
+        return card
+    else:
+        return "Card already exists"
