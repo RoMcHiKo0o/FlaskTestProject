8ec972f7db1ffb53efeaa6cc1e79f404b4a64e0c
roman <roman.golubovich@gmail.com>
2023-02-19 16:42:51 +0300

    updated file for creating collections and inserting data (alternative for export)

--- db.js
+++ db.js
@@ -1,120 +1,161 @@
 use test_database;
 
-//db.createCollection("orders")
-//db.createCollection("products")
-//db.createCollection("cards")
+db.createCollection("orders")
+db.createCollection("products")
+db.createCollection("cards")
+db.createCollection("deleted_cards")
+
+ db.products.insertMany([
+     {
+         "_id" : ObjectId("63ef4839a6a36ecd62717f00"),
+         'name': "milk",
+         "price": 2
+     },
+     {
+         "_id" : ObjectId("63ef4839a6a36ecd62717f01"),
+         'name': "bread",
+         "price": 2,
+     },
+     {
+         "_id" : ObjectId("63ef4839a6a36ecd62717f02"),
+         'name': "butter",
+         "price": 5
+     },
+     {
+         "_id" : ObjectId("63ef4839a6a36ecd62717f03"),
+         'name': "gum",
+         "price": 1
+     },
+     {
+         "_id" : ObjectId("63ef4839a6a36ecd62717f04"),
+         'name': "pizza",
+         "price": 10
+     },
+ ])
+
+ db.orders.insertMany([
+     {
+         "_id" : ObjectId("63f2173fcb5528905e90e5ef"),
+         'date': new ISODate('2023-02-15T10:00:00Z'),
+         'price': 10,
+         'discount': 3.5,
+         'total': 9.65,
+         'products': [
+             {
+                 "id": ObjectId('63ef4839a6a36ecd62717f00'),
+                 'amount': 2
+             },
+             {
+                 "id": ObjectId('63ef4839a6a36ecd62717f02'),
+                 'amount': 1
+             },
+             {
+                 "id": ObjectId('63ef4839a6a36ecd62717f03'),
+                 'amount': 1
+             },
+         ]
+     },
+     {
+         "_id" : ObjectId("63f2173fcb5528905e90e5f0"),
+        'date': new ISODate('2023-02-06T10:00:00Z'),
+        'price': 20,
+        'discount': 5,
+        'total': 19.0,
+         'products': [
+             {
+                 "id": ObjectId('63ef4839a6a36ecd62717f04'),
+                 'amount': 2
+             }
+         ]
+     },
+     {
+         "_id" : ObjectId("63f2173fcb5528905e90e5f1"),
+         'date': new ISODate('2023-02-07T10:00:00Z'),
+         'price': 4,
+         'discount': 5,
+         'total': 3.8,
+         'products': [
+             {
+                 "id": ObjectId('63ef4839a6a36ecd62717f00'),
+                 'amount': 1
+             },
+             {
+                 "id": ObjectId('63ef4839a6a36ecd62717f01'),
+                 'amount': 1
+             }
+         ]
+     },
+     {
+         "_id" : ObjectId("63f2173fcb5528905e90e5f2"),
+         'date': new ISODate('2023-02-08T10:00:00Z'),
+         'price': 1,
+         'discount': 5,
+         'total': 0.95,
+         'products': [
+             {
+                 "id": ObjectId('63ef4839a6a36ecd62717f03'),
+                 'amount': 1
+             }
+         ]
+     },
+ ])
 
 
 
-db.orders.insertMany([
-    {
-       'number': '6226',
-       'date': new ISODate('2023-02-15T10:00:00Z'),
-       'price': 10,
+db.cards.insertMany([
+   {
+       'series':'1',
+       'number':'1-0000',
+       'create_date':new ISODate('2023-02-14T10:00:00Z'),
+       'start_date':new ISODate('2023-02-14T10:00:00Z'),
+       'end_date':new ISODate('2023-03-14T10:00:00Z'),
+       'recent_buy_date':new ISODate('2023-02-15T10:00:00Z'),
+       'buy_counter':1,
+       'card_state':"Activated",
        'discount': 3.5,
-       'total': 9.65    },
-    {
-       'number': '0000',
-       'date': new ISODate('2023-02-06T10:00:00Z'),
-       'price': 20,
+       'orders': [ObjectId('63f2173fcb5528905e90e5ef')]
+   },
+   {
+       'series':'1',
+       'number':'1-0001',
+       'create_date':new ISODate('2023-01-09T10:00:00Z'),
+       'start_date':new ISODate('2023-01-09T10:00:00Z'),
+       'end_date':new ISODate('2023-02-09T10:00:00Z'),
+       'recent_buy_date':new ISODate('2023-02-08T10:00:00Z'),
+       'buy_counter':3,
+       'card_state':"Expired",
        'discount': 5,
-       'total': 19.0
-    },
+       'orders': [
+           ObjectId('63f2173fcb5528905e90e5f0'),
+           ObjectId('63f2173fcb5528905e90e5f1'),
+           ObjectId('63f2173fcb5528905e90e5f2')
+       ]
+   },
+   {
+       'series':'2',
+       'number':'2-0000',
+       'create_date':new ISODate('2023-02-12T10:00:00Z'),
+       'start_date': '',
+       'end_date': '',
+       'recent_buy_date':'',
+       'buy_counter':0,
+       'card_state':"Not activated",
+       'discount': 1,
+       'orders': []
+   },
     {
-       'number': '0001',
-       'date': new ISODate('2023-02-07T10:00:00Z'),
-       'price': 10,
-       'discount': 5,
-       'total': 9.5
-    },
-    {
-       'number': '0083',
-       'date': new ISODate('2023-02-08T10:00:00Z'),
-       'price': 1,
-       'discount': 5,
-       'total': 4.75
-    },
-])
-
-db.products.insertMany([
-    {
-        'name': "milk",
-        "price": 2    },
-    {
-        'name': "bread",
-        "price": 2,
-    },
-    {
-        'name': "butter",
-        "price": 5
-    },
-    {
-        'name': "gum",
-        "price": 1
-    },
-    {
-        'name': "pizza",
-        "price": 10
-    },    
-])
-
-
-//db.cards.insertMany([
-//    {
-//        'series':'1',
-//        'number':'9396',
-//        'create_date':new ISODate('2023-02-14T10:00:00Z'),
-//        'start_date':new ISODate('2023-02-14T10:00:00Z'),
-//        'end_date':new ISODate('2023-03-14T10:00:00Z'),
-//        'recent_buy_date':new ISODate('2023-02-15T10:00:00Z'),
-//        'buy_counter':1,
-//        'card_state':"Activated",
-//        'discount': 3.5,
-//        'orders': [ObjectId('63ed3a169a8f0b9eee7b42ce')]//    },
-//    {
-//        'series':'1',
-//        'number':'9200',
-//        'create_date':new ISODate('2023-01-09T10:00:00Z'),
-//        'start_date':new ISODate('2023-01-09T10:00:00Z'),
-//        'end_date':new ISODate('2023-02-09T10:00:00Z'),
-//        'recent_buy_date':new ISODate('2023-02-08T10:00:00Z'),
-//        'buy_counter':3,
-//        'card_state':"Expired",
-//        'discount': 5,
-//        'orders': [
-//            ObjectId('63ed3a169a8f0b9eee7b42cf'),
-//            ObjectId('63ed3a169a8f0b9eee7b42d0'),
-//            ObjectId('63ed3a169a8f0b9eee7b42d1')//        ]
-//    },
-//    {
-//        'series':'2',
-//        'number':'4299',
-//        'create_date':new ISODate('2023-02-12T10:00:00Z'),
-//        'start_date': null,
-//        'end_date': null,
-//        'recent_buy_date':null,
-//        'buy_counter':0,
-//        'card_state':"Not activated",
-//        'discount': 1,
-//        'orders': []
-//    },//])
-
-
-db.cards.insertOne({
         'series':'2',
-        'number':'1111',
+        'number':'2-0001',
         'create_date':new ISODate('2023-02-12T10:00:00Z'),
-        'start_date': null,
-        'end_date': null,
-        'recent_buy_date':null,
+        'start_date': '',
+        'end_date': '',
+        'recent_buy_date':'',
         'buy_counter':0,
         'card_state':"Not activated",
         'discount': 1,
         'orders': []
-    })
-
-
-db.createCollection("deleted_cards")
+    }
+])
 
 db.cards.find({})
 
@@ -124,5 +165,3 @@ db.products.find({})
 
 db.deleted_cards.find({})
 
-
-db.cards.find({'number': '1111', discount})
