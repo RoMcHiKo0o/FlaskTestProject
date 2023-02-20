d42b012c316d175e4540baead1e46d90e2340716
roman <roman.golubovich@gmail.com>
2023-02-20 11:18:44 +0300

    added card generate, added get_orders and get_products

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from bson import json_util
 from flask import Flask, request
+from datetime import datetime
 from crud import *
 from database import db
-from bson import json_util
 
 app = Flask(__name__)
 
@@ -51,6 +52,34 @@ def rollbackCard(number):
 def filterCards():
     cards = get_cards_list(db, dict(request.args))
     return json_util.dumps({"message": cards})
+
+
+@app.route('/generate/', methods=['POST'])
+def createCards():
+    post = request.json
+    print(post)
+    if datetime.fromisoformat(post['start_date']) < datetime.now():
+        post['card_state'] = "Not activated"
+    elif datetime.fromisoformat(post['end_date']) < datetime.now():
+        return json_util.dumps({'message': "End date should be earlier than now"})
+    else:
+        post['card_state'] = "Activated"
+    if 'discount' not in post:
+        post['discount'] = 2.0
+    res = generate_cards(db, request.json)
+    return json_util.dumps({'message': res})
+
+
+@app.route('/cards/<number>/orders/')
+def getCardOrders(number):
+    orders = get_orders(db, number)
+    return json_util.dumps({"message": orders})
+
+
+@app.route('/orders/<order_id>/products/')
+def getProducts(order_id):
+    products = get_products(db, order_id)
+    return json_util.dumps({"message": products})
 
 
 if __name__ == "__main__":
--- crud.py
+++ crud.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+from datetime import timedelta
+
+from bson import ObjectId
+
 DEFAULT_FIELDS = {'_id': 0, 'series': 1, 'number': 1, 'create_date': 1, 'end_date': 1, 'card_state': 1}
 
 
@@ -11,6 +16,10 @@ def convert_type(field):
     return None
 
 
+def generate_number(n):
+    return f"{n[:2]}{int(n[2:]) + 1:04}"
+
+
 def get_cards(db):
     return [i for i in db.cards.find({}, {'_id': 0})]
 
@@ -18,11 +27,13 @@ def get_cards(db):
 def get_cards_list(db, filter_search=None, fields=None):
     if fields is None:
         fields = DEFAULT_FIELDS
+
     if filter_search is None:
         fields = {}
-    for k, v in filter_search.items():
-        if (tpe := convert_type(k)) is not None:
-            filter_search[k] = tpe(v)
+    else:
+        for k, v in filter_search.items():
+            if (tpe := convert_type(k)) is not None:
+                filter_search[k] = tpe(v)
     return [i for i in db.cards.find(filter_search, fields)]
 
 
@@ -38,7 +49,11 @@ def get_card_state(db, number):
 def activate_card(db, number):
     state = get_card_state(db, number)
     if state == "Not activated":
-        db.cards.update_one({'number': number}, {'$set': {'card_state': "Activated"}})
+        db.cards.update_one({'number': number}, {'$set': {
+            'card_state': "Activated",
+            "start_date": datetime.now(),
+            "end_date": datetime.now() + timedelta(days=30),
+        }})
         return "Card was successfully activated"
     elif state == "Expired":
         return 'Card is expired'
@@ -49,7 +64,11 @@ def activate_card(db, number):
 def deactivate_card(db, number):
     state = get_card_state(db, number)
     if state == "Activated":
-        db.cards.update_one({'number': number}, {'$set': {'card_state': "Not activated"}})
+        db.cards.update_one({'number': number}, {'$set': {
+            'card_state': "Not activated",
+            "start_date": '',
+            "end_date": '',
+        }})
         return "Card was successfully deactivated"
     elif state == "Expired":
         return 'Card is expired'
@@ -90,3 +109,55 @@ def insert_deleted_card(db, card):
         return card
     else:
         return "Card already exists"
+
+
+def generate_cards(db, json):
+    tmp = get_cards_list(db, filter_search={'series': json['series']}, fields={"_id": 0, "number": 1})
+    number = f"{json['series']}-0000"
+    last_number = 0
+    if len(tmp) != 0:
+        last_number = max([i['number'] for i in tmp])
+    for i in range(json['amount']):
+        if last_number != 0:
+            number = generate_number(last_number)
+        last_number = number
+        card = {
+            "series": json['series'],
+            "number": number,
+            "orders": [],
+            "buy_counter": 0,
+            "discount": json['discount'],
+            "create_date": datetime.now(),
+            "start_date": json['start_date'],
+            "end_date": json['end_date'],
+            'card_state': json['card_state']
+        }
+        db.cards.insert_one(card)
+    return "Cards have been added"
+
+
+def get_order_by_id(db, order_id):
+    if not isinstance(order_id, ObjectId):
+        order_id = ObjectId(order_id)
+    return next(db.orders.find({"_id": order_id}, {"_id": 0}), {})
+
+
+def get_orders(db, number):
+    order_list = get_card_by_number(db, number)['orders']
+    return [get_order_by_id(db, order) for order in order_list]
+
+
+def get_product_by_id(db, prod_id, fields=None):
+    if fields is None:
+        fields = {}
+    if not isinstance(prod_id, ObjectId):
+        prod_id = ObjectId(prod_id)
+    return next(db.products.find({"_id": prod_id}, fields), {})
+
+
+def get_products(db, order_id):
+    order = get_order_by_id(db, order_id)
+    if order == {}:
+        return "No order with that id"
+    prod_list = order['products']
+    return [{**get_product_by_id(db, prod['id'], {"_id": 0, "name": 1}), "amount": prod['amount']} for prod in prod_list]
