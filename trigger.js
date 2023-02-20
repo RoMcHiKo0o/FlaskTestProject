afc14421d51ebfa619e5e49c8f95bf1f8e32bdfd
roman <roman.golubovich@gmail.com>
2023-02-20 22:08:42 +0300

    updated timezone offset for UTC+3

--- trigger.js
+++ trigger.js
@@ -1,5 +1,5 @@
 exports = function() {
 
   const cards = context.services.get("Cluster0").db("test_database").collection("cards");
-  return cards.updateMany({"card_state": "Activated", "end_date": {$lt: new Date()}},{$set: {"card_state": "Expired"}});
+  return cards.updateMany({"card_state": "Activated", "end_date": {$lt: new Date(3*3600*1000)}},{$set: {"card_state": "Expired"}});
 };
